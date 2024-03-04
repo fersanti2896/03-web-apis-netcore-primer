8cbee6736bfcd6e59f0dd2808cdcfbdbd097d849
Fernando Nicolás <fersanti2896@gmail.com>
2024-03-04 13:20:25 -0600

    Creando attributos de HATEOAS

---
+++ Utilidades/AttributeHATEOAS.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AutoresAPI.Utilidades {
+    public class AttributeHATEOAS : ResultFilterAttribute {
+        protected bool incluirHateoas(ResultExecutingContext context) { 
+            var result = context.Result as ObjectResult;
+
+            if (!respSuccess(result)) { 
+                return false;
+            }
+
+            var header = context.HttpContext.Request.Headers["incluirHateoas"];
+
+            if (header.Count == 0) { 
+                return false;
+            }
+
+            var valor = header[0];
+
+            if(!valor.Equals("Y", StringComparison.InvariantCultureIgnoreCase)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool respSuccess(ObjectResult result) {
+            if (result is null || result.Value == null) { 
+                return false;
+            }
+
+            if (result.StatusCode.HasValue && !result.StatusCode.Value.ToString().StartsWith("2")) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Utilidades/AutorAttributeHATEOAS.cs
@@ -0,0 +1,21 @@
+﻿using AutoresAPI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AutoresAPI.Utilidades {
+    public class AutorAttributeHATEOAS : AttributeHATEOAS {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next) {
+            var exists = incluirHateoas(context);
+
+            if (!exists) { 
+                await next();
+                return;
+            }
+
+            var result = context.Result as ObjectResult;
+            var model = result.Value as AutorDTO ?? throw new ArgumentException("Se esperaba una instancia de AutorDTO");
+            
+            await next();
+        }
+    }
+}
